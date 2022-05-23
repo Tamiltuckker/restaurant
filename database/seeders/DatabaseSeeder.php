af9c8e92dc735b16323bba6400cd57d91cac3436
tamil <tamilselvan.k@arkinfotec.com>
2022-05-23 16:15:37 +0530

    changes 007

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            CreateAdminUserSeeder::class,
+            PermissionTableSeeder::class,
             TableSeeder::class,
+            ContentSeeder::class,
+            AboutUsSeeder::class,
         ]);
     }
 }
